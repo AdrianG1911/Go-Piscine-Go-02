4aed6cb3784f2f14c391ecb3525197918479de02
Adrian gutierrez <19gutierreza@gmail.com>
2025-03-25 12:40:39 +0900

    overflows do not need to be delt with

--- ex02/vendor/piscine/iterativepower.go
+++ ex02/vendor/piscine/iterativepower.go
@@ -3,11 +3,15 @@ package piscine
 func IterativePower(nb int, power int) int {
 	ans := 1
 
+	if power < 0 || nb == 0 {
+		return (0)
+	}
 	if power == 0 {
-		return (1);
+		return (1)
 	}
-	if power < 0 {
-		return (0);
+	for power > 0 {
+		ans = ans * nb
+		power--
 	}
-
+	return ans
 }
